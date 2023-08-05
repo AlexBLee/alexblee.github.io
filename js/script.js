96ee4eb6740e8bbdd4c84e7aa41c627fc2cfda05
alee <alee@popreach.com>
2023-08-05 13:58:34 -0700

    Testing out modal box

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+function showModal(caption, imageSrc) {
+  var modal = document.getElementById("modal");
+  var modalImage = document.getElementById("modalImage");
+  var modalCaption = document.getElementById("modalCaption");
+
+  modalImage.src = imageSrc;
+  modalCaption.textContent = caption;
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
